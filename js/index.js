85420ef65931b012b2021e6607fa9a15a0d38e7c
MasterFengYu <52442578+MasterFengYu@users.noreply.github.com>
2021-07-12 00:58:25 +0800

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,62 @@
+
+let width;
+let height;
+let scene;
+let camera;
+let renderer;
+let control;
+let player;
+let listener;
+let isRunning;
+
+let cameraOffset = {
+	x: 0,
+	y: 120,
+	z: -60
+};
+
+// let gui = new dat.GUI();
+
+let playerConfig = {
+  MaxSpeed: 20, 
+  AccelSpeed: 5, 
+  turnSpeed: 45,
+  DampingSpeed: 3,
+  CameraFollow: true,
+};
+// gui.add(playerConfig, 'MaxSpeed', 0, 60, 0.5);
+// gui.add(playerConfig, 'AccelSpeed', 2, 40, 0.1);
+// gui.add(playerConfig, 'turnSpeed', 1, 90, 0.5);
+// gui.add(playerConfig, 'DampingSpeed', 1, 30, 0.1);
+// gui.add(playerConfig, 'CameraFollow');
+
+
+let speed = 0;
+let accel = 0;
+let turn = 0;
+
+loadAssets(AssetConfig, () => {
+
+	initUI();
+
+	initScene();
+
+	loadMap();
+
+	loadBlocks();
+
+	prepareLevel();
+
+	animate();
+
+// *****************************************************
+// !!!!!!!!!!!!!!!!!!!!!!!!
+	introBgmSound = new THREE.Audio( listener );
+	introBgmSound.setBuffer( assets['intro_bgm'] );
+	introBgmSound.setLoop( true );
+	introBgmSound.setVolume(1);
+	introBgmSound.play();
+// *****************************************************
+
+});
+
